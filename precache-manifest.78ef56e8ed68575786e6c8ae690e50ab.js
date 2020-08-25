30d4c884d162fdb657f877b511d022c099fc35d1
Vladimir <vovamip@ya.ru>
2020-08-25 19:19:18 +0300

    Updates

---
+++ precache-manifest.78ef56e8ed68575786e6c8ae690e50ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2edef32ab95c601243f3145c999fad1",
+    "url": "/beer-shop/index.html"
+  },
+  {
+    "revision": "cacea037bb44ad640e42",
+    "url": "/beer-shop/static/css/main.20914552.chunk.css"
+  },
+  {
+    "revision": "02967345e56b11174e5d",
+    "url": "/beer-shop/static/js/2.98640765.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/beer-shop/static/js/2.98640765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cacea037bb44ad640e42",
+    "url": "/beer-shop/static/js/main.9609f57c.chunk.js"
+  },
+  {
+    "revision": "b9b159ad41b352136cda",
+    "url": "/beer-shop/static/js/runtime-main.faa2a315.js"
+  }
+]);
